3c9abf8866d96e362809ba031c50037b1223f021
TCRicks <122693985+TCRicks@users.noreply.github.com>
2023-03-27 17:51:04 -0500

    Update SidebarBar.js

--- RockNRoboticsWebsite-main/Website/code/SidebarBar.js
+++ RockNRoboticsWebsite-main/Website/code/SidebarBar.js
@@ -6,11 +6,11 @@ class Sidebar extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
         <div class="sidebarDiv" id="sidebarDiv">
-            <button class="sidebarButton" id="sidebarButton"><img class="hamburger" src="images/icons/hamburger-menu.svg" alt="Sidebar"></button>
-            <button class="homeButton"onclick="window.location.href='index.html'">Home</button>
-            <button class="aboutUsButton" onclick="window.location.href='AboutUs.html'">About Us</button>
-            <button class="roboCamButton" onclick="window.location.href='RoboCam.html'">Robo Cam</button>
-            <button class="sponsorsButton" onclick="window.location.href='Sponsors.html'">Sponsors</button>
+            <button class="sidebarButton" id="sidebarButton"><img class="hamburger" src="../images/icons/hamburger-menu.svg" alt="Sidebar"></button>
+            <button class="homeButton"onclick="window.location.href='../index.html'">Home</button>
+            <button class="aboutUsButton" onclick="window.location.href='../AboutUs.html'">About Us</button>
+            <button class="roboCamButton" onclick="window.location.href='../RoboCam.html'">Robo Cam</button>
+            <button class="sponsorsButton" onclick="window.location.href='../Sponsors.html'">Sponsors</button>
         </div>
         `;
     }
